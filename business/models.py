d109481e9b1f956f15bf1b01c6aece4c1b7cdcc6
sngina <sngina707@gmail.com>
2021-07-24 10:59:01 +0300

    add class neighbourhood

--- business/models.py
+++ business/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.shortcuts import render , redirect
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    occupants = models.OneToOneField(User , on_delete=models.CASCADE , null= True)
+
+    def __str__(self):
+        return self.name
+
+    def save_neighbourhood(self):
+        self.save()    
+
+#delete function
+    @classmethod 
+    def delete_neighbourhood(cls , id):
+        cls.objects.filter(id=id).delete()
+
+#search function
+    @classmethod
+    def search_neighbourhood(cls ,neighbourhood ):
+        name = cls.objects.filter(location_name = neighbourhood)
+
+#update function
+    @classmethod
+    def update(cls , id , update):
+        neighbourhood=cls.objects.filter(id = id).update(neighbourhood =update)
+
+    @classmethod
+    def update(cls , id , update):
+        occupants = cls.objects.filter(id=id).update(occupants = update)
+    
