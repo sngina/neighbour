7108d6dd7e342b4b256c8dd345d094121c979eb8
sngina <sngina707@gmail.com>
2021-07-26 12:42:18 +0300

    add admin page

--- business/admin.py
+++ business/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from.models import Neighbourhood ,Business ,Profile
 # Register your models here.
+
+class NeighbourhoodAdmin(admin.ModelAdmin):
+    list_display = ('name' , 'location' , 'occupants')
+
+class BusinessAdmin(admin.ModelAdmin):
+    filter_horizontal = ('image' ,)
+
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
